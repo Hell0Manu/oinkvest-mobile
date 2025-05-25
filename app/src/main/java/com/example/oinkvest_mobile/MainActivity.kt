462792fd433aff941c9d9390166017a6ace0d2f3
Emanuelle <102062744+Hell0Manu@users.noreply.github.com>
2025-05-25 17:25:44 -0300

    navigation bar funcionando 100% faltando apenas estilos da prototipação

--- app/src/main/java/com/example/oinkvest_mobile/MainActivity.kt
+++ app/src/main/java/com/example/oinkvest_mobile/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.oinkvest_mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.example.oinkvest_mobile.navigation.navhost.MyNavHost
 import com.example.oinkvest_mobile.ui.theme.OinkvestmobileTheme
@@ -11,7 +10,7 @@ import com.example.oinkvest_mobile.ui.theme.OinkvestmobileTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             OinkvestmobileTheme {
                 MyNavHost(navHostController = rememberNavController())
--- app/src/main/java/com/example/oinkvest_mobile/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/oinkvest_mobile/main/home/HomeScreen.kt
@@ -3,12 +3,16 @@ package com.example.oinkvest_mobile.main.home
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,12 +22,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.oinkvest_mobile.navigation.item.BottomNavItem
 import com.example.oinkvest_mobile.navigation.navhost.BottomAppBarNavHost
+import com.example.oinkvest_mobile.navigation.navhost.FeedScreenRoute
+import com.example.oinkvest_mobile.navigation.navhost.FriendsScreenRoute
+import com.example.oinkvest_mobile.navigation.navhost.ProfileScreenRoute
+import com.example.oinkvest_mobile.navigation.navhost.SearchScreenRoute
 
 @Composable
 fun HomeScreen(navHostController: NavHostController = rememberNavController()) {
@@ -41,11 +52,43 @@ fun HomeScreen(navHostController: NavHostController = rememberNavController()) {
                 containerColor = Color.Black,
                 content = {
                     BottomNavItem.items.forEachIndexed { index, item ->
+                        if (index == 0){
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
                         AddBottomItem(
                             item = item,
                             selected = selectedItem == index,
-                            onClick = { selectedItem = index }
+                            onClick = {
+                                selectedItem = index
+                                when(index) {
+                                    0 -> navHostController.navigate(FeedScreenRoute)
+                                    1 -> navHostController.navigate(SearchScreenRoute)
+                                    2 -> navHostController.navigate(FriendsScreenRoute)
+                                    3 -> navHostController.navigate(ProfileScreenRoute)
+                                }
+                            },
+                            label = {
+                                if(selectedItem == index) {
+                                    Text(
+                                        text = stringResource(id = item.title),
+                                        style = TextStyle(
+                                            color = Color.White,
+                                            fontSize = 12.sp,
+                                            fontWeight = if (selectedItem == index) {
+                                                androidx.compose.ui.text.font.FontWeight.Bold
+                                            } else {
+                                                androidx.compose.ui.text.font.FontWeight.Normal
+                                            }
+                                        )
+
+                                    )
+                                }
+                            }
                         )
+
+                        if (index == BottomNavItem.items.size - 1){
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
                     }
                 }
             )
@@ -76,9 +119,17 @@ fun RowScope.AddBottomItem(
         icon = {
             Icon(
                 painter = painterResource(id = if (selected) item.selectedIcon else item.unSelectedIcon),
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
             )
-        }
+        },
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color.White,
+            unselectedIconColor = Color.Gray,
+            indicatorColor = Color.Black
+
+        )
     )
 
 }
